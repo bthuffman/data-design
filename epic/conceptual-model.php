4f9721111b77acea844433c905e21e36a7ee9325
Brandon Huffman <bt_huffman@msn.com>
2019-01-17 10:31:43 -0700

    Alphabetized foreign keys and other attributes

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -10,39 +10,40 @@
 		<h3>Workspaces</h3>
 		<ul>
 			<li>workspaceId (primary key)</li>
-			<li>workspaceMemberId (foreign key)</li>
 			<li>workspaceChannelId (foreign key)</li>
 			<li>workspaceDirectMessagesId(foreign key)</li>
-			<li>workspaceThreads (theoretically would be entity unto itself [therefore foreign key] if entities could be > 4.)</li>
+			<li>workspaceMemberId (foreign key)</li>
 			<li>workspaceApps (theoretically would be entity unto itself [therefore foreign key] if entities could be > 4.)</li>
+			<li>workspaceThreads (theoretically would be entity unto itself [therefore foreign key] if entities could be > 4.)</li>
 		</ul>
 		<h3>Members </h3>
 		<ul>
 			<li>memberId (primary key)</li>
-			<li>memberName</li>
+			<li>memberWorkspaceId (foreign key)</li>
 			<li>memberDisplayName</li>
-			<li>memberTimeZone</li>
-			<li>memberPhoneNumber</li>
 			<li>memberEmail</li>
 			<li>membersFavoriteCaptain</li>
 			<li>membersGitHubProfile</li>
-			<li>membersStatus</li>
+			<li>memberHash</li>
+			<li>memberName</li>
 			<li>memberOnlineStatus</li>
-			<li>memberWorkspaceId (foreign key)</li>
+			<li>memberPhoneNumber</li>
+			<li>membersStatus</li>
+			<li>memberTimeZone</li>
 		</ul>
 		<h3>Channels</h3>
 		<ul>
 			<li>channelId (primary key)</li>
-			<li>channelTitle</li>
-			<li>channelPurpose</li>
-			<li>channelCreatedDetails</li>
-			<li>channelRelatedChannels (by channelId?)</li>
-			<li>channelHighlights</li>
-			<li>channelPinnedItems</li>
 			<li>channelMembersId (foreign key)</li>
 			<li>channelWorkspaceId (foreign key)</li>
 			<li>channelApps (theoretically would be entity unto itself [therefore foreign key] if entities could be > 4.)</li>
+			<li>channelCreatedDetails</li>
+			<li>channelHighlights</li>
+			<li>channelPinnedItems</li>
+			<li>channelPurpose</li>
+			<li>channelRelatedChannels (by channelId?)</li>
 			<li>channelSharedFiles</li>
+			<li>channelTitle</li>
 		</ul>
 		<h3>Direct Messages</h3>
 		<ul>
